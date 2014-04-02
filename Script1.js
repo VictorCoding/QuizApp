336becd8ea6192b6831f288ce1032dc2d12b22b4
VictorCoding <v_tur1@hotmail.com>
2014-04-01 20:56:06 -0500

    First Commit

---
+++ Script1.js
@@ -0,0 +1,20 @@
+/*jslint browswer: true*/
+(function () {
+    "use strict";
+
+    var radio1 = document.createElement('input');
+    radio1.type = 'radio';
+    radio1.id = 'radio1one';
+
+    var label1 = 
+    document.getElementById("firstQuestion");
+
+
+
+
+
+
+
+
+
+}())
